a6bd135e49b3bfc22a07906a650afd03a57a9d50
craigmindset <irediacraig@gmail.com>
2025-08-23 04:32:30 +0100

    bug fix username display, dashboard protection

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,5 +1,18 @@
-import Welcome from "@/components/welcome"
+"use client";
+import Welcome from "@/components/welcome";
+import { useUser } from "@/lib/services/user/user";
+import { useEffect, useState } from "react";
 
 export default function WelcomePage() {
-  return <Welcome username="Kred" />
+  const { user } = useUser();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const username = user?.firstname || "User";
+  return <Welcome username={username} />;
 }
