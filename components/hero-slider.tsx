57b7054c866b1c2ec35793998d3ee3af3f9f9ca6
craigmindset <irediacraig@gmail.com>
2025-10-18 13:32:17 +0100

    updated hero

--- components/hero-slider.tsx
+++ components/hero-slider.tsx
@@ -66,7 +66,7 @@ export default function HeroSection() {
       <video
         ref={videoRef}
         className="absolute inset-0 h-full w-full object-cover z-0"
-        src="https://hlfwfvupabrc8fwr.public.blob.vercel-storage.com/Kredmart-video.mp4"
+        src="https://hlfwfvupabrc8fwr.public.blob.vercel-storage.com/home-kredmart.mp4"
         autoPlay
         muted
         loop
